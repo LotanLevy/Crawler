18f54491cb98ac2a4bdb99caee6ae6962b5e3785
LotanLevy <lotan.levy@mail.huji.ac.il>
2020-06-23 17:48:50 +0300

    "continue the images loading when some image cant be found"

--- amazon_data_parser/AmazonDataParser.py
+++ amazon_data_parser/AmazonDataParser.py
@@ -81,14 +81,16 @@ def save_images(outputpath, urls_labels_map_file):
 
             if not os.path.exists(path):
                 os.makedirs(path)
-                print(path)
+
 
             try:
                 urllib.request.urlretrieve(line_items[0].strip(),
                                            os.path.join(path, "{}.png".format(image_num)))
+                image_num += 1
+                print(line)
+
             except urllib.error.HTTPError:
                 continue
-            image_num += 1
 
 
 
--- mergeDataFiles.py
+++ mergeDataFiles.py
@@ -1,6 +1,15 @@
 
 import sys
 
+def insert_lines(dest, first, second):
+    lines = []
+    with open(first, 'r') as ff:
+        lines += ff.readlines()
+    with open(second, 'r') as sf:
+        lines += sf.readlines()
+
+    with open(dest, 'w') as df:
+        df.writelines(lines)
 
 
 
@@ -9,5 +18,6 @@ if __name__ == "__main__":
     first_file = sys.argv[1]
     second_file = sys.argv[2]
     dest_path = sys.argv[3]
+    insert_lines(dest_path, first_file, second_file)
 
 
