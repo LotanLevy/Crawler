84ab0f3e9e15efb169a560ef76c81db2f0c8c64e
LotanLevy <lotan.levy@mail.huji.ac.il>
2020-06-23 18:13:29 +0300

    "continue the images loading when some image cant be found"

--- amazon_data_parser/AmazonDataParser.py
+++ amazon_data_parser/AmazonDataParser.py
@@ -71,13 +71,23 @@ class Parser:
                     continue
 
 
-def save_images(outputpath, urls_labels_map_file):
+def save_images(outputpath, urls_labels_map_file, max_items_for_cls, classes):
     main_path = os.path.join(outputpath, "amazon_dataset")
+    item_num_dict = dict()
     image_num=0
     with open(urls_labels_map_file, 'r') as file:
         for line in file:  # Read one line.
             line_items = line.split(" ")
-            path = os.path.join(main_path, line_items[1].strip())
+            label = line_items[1].strip()
+            if label not in classes:
+                continue
+            path = os.path.join(main_path, label)
+            if label in item_num_dict:
+                if item_num_dict[label] > max_items_for_cls:
+                    continue
+                item_num_dict[label] += 1
+            else:
+                item_num_dict[label] = 1
 
             if not os.path.exists(path):
                 os.makedirs(path)
@@ -87,7 +97,6 @@ def save_images(outputpath, urls_labels_map_file):
                 urllib.request.urlretrieve(line_items[0].strip(),
                                            os.path.join(path, "{}.png".format(image_num)))
                 image_num += 1
-                print(line)
 
             except urllib.error.HTTPError:
                 continue
@@ -96,18 +105,27 @@ def save_images(outputpath, urls_labels_map_file):
 
 def get_config():
     parser = argparse.ArgumentParser(description='Parse Amazon json file')
+    parser.add_argument('--parse', '-p', action="store_true", default=False, help='parse for urls')
+    parser.add_argument('--download', '-d', action="store_true", default=False, help='download images')
+
+
     parser.add_argument('--inputpath', '-ip', help='File to parse path')
     parser.add_argument('--outputpath', '-op', help='Directory for the outputs')
     parser.add_argument('--maxlines', '-ml', default=-1, type=int, help='Maximum lines to parse')
+    parser.add_argument('--maxforcls', '-mc', default=1000, type=int, help='Maximum items for cls')
+    parser.add_argument('--relevantcls', '-l', help='a list of the relevant labels')
+
+
+
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = get_config()
     if not os.path.exists(args.outputpath):
         os.makedirs(args.outputpath)
-    # p = Parser(args.inputpath, args.maxlines)
-    # p.write_items(args.outputpath)
-
-
-
-    save_images(args.outputpath, os.path.join(args.outputpath, "urlfile.txt"))
+    if args.parse:
+        p = Parser(args.inputpath, args.maxlines)
+        p.write_items(args.outputpath)
+    if args.download:
+        classes = args.relevantcls.split(",")
+        save_images(args.outputpath, os.path.join(args.outputpath, "urlfile.txt"), args.maxforcls, classes)
